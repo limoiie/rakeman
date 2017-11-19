55478c45ba7aa279f3d42a2aac8d64b372cd8790
limo <iie4limo@163.com>
2017-11-19 23:08:15 +0800

    <add>: Add jquery-dev and material-dev

--- rake/static/rake/js/rakeman.js
+++ rake/static/rake/js/rakeman.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    $('.example').stickMe(); // Yes, that's it!
-});
+  var ele = $('.sticky-row')
+  ele.stickMe();
+  ele.on('sticky-begin', function() {
+    console.log("Began");
+  })
+  ele.on('sticking', function() {
+    console.log("Sticking");
+  })
+  ele.on('top-reached', function() {
+    console.log("Top reached");
+  })
+  ele.on('bottom-reached', function() {
+    console.log("Bottom reached");
+  })
+})
