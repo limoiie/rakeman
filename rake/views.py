7c79b6daab3fa138429303563e9d745910c33ecd
limoiie <limo.iie4@gmail.com>
2018-01-12 11:58:39 +0800

    <update>: Update Ui module

--- rake/views.py
+++ rake/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.utils import timezone
 
-from rake.models import QueryResultItem
+from rake.core.query import *
 
 
 def index(request):
@@ -11,24 +10,30 @@ def index(request):
 
 
 # todo
+# def fetch_hot_words(num)->list:
+#     words = list()
+#     for i in range(0, num):
+#         words.append({'word': 'word'+str(i), 'freq': i})
+#     return words
+
+
 def query(request, query_string, page):
-    raw_query_string = query_string
-    query_string = query_string[1:len(query_string)-1]
-    results = []
-    for i in range(1, 10):
-        results.append(QueryResultItem(
-            title='News Title',
-            url='http://www.baidu.com',
-            digest='Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n'
-                      'Mauris sagittis pellentesque lacus eleifend lacinia...',
-            keywords='keyword',
-            source='XinHua',
-            datetime=timezone.now()
-        ))
-    context = {
-        "query_string": raw_query_string,
-        "current_page": int(page),
-        "total_page": range(0, 6),
-        "results": results
-    }
-    return HttpResponse(render(request, 'rake/news_list.html', context))
+    # raw_query_string = query_string
+    # query_string = query_string[1:len(query_string)-1]
+    #
+    # cost = time.time()
+    # results, total_num = express_query(query_string, int(page), 10)
+    # state = '为您找到 %d 条新闻，共耗时 %f 秒' % (total_num, time.time()-cost)
+    #
+    # context = {
+    #     "raw_query_string": raw_query_string,
+    #     "query_string": query_string,
+    #     "current_page": int(page),
+    #     "total_page": range(0, 6),
+    #     "results": results,
+    #     'query_state': state
+    # }
+    # return HttpResponse(render(request, 'rake/news_list.html', context))
+
+    context = {"items": range(1, 10)}
+    return HttpResponse(render(request, 'rake/index.html', context))
