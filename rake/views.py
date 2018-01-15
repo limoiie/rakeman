cc2158eb7d58e71636681f3f03d9668200930e44
limoiie <limo.iie4@gmail.com>
2018-01-15 22:47:38 +0800

    <update>: Update search box

--- rake/views.py
+++ rake/views.py
@@ -18,22 +18,22 @@ def index(request):
 
 
 def query(request, query_string, page):
-    # raw_query_string = query_string
-    # query_string = query_string[1:len(query_string)-1]
-    #
-    # cost = time.time()
-    # results, total_num = express_query(query_string, int(page), 10)
-    # state = '为您找到 %d 条新闻，共耗时 %f 秒' % (total_num, time.time()-cost)
-    #
-    # context = {
-    #     "raw_query_string": raw_query_string,
-    #     "query_string": query_string,
-    #     "current_page": int(page),
-    #     "total_page": range(0, 6),
-    #     "results": results,
-    #     'query_state': state
-    # }
-    # return HttpResponse(render(request, 'rake/news_list.html', context))
+    raw_query_string = query_string
+    query_string = query_string[1:len(query_string)-1]
 
-    context = {"items": range(1, 10)}
-    return HttpResponse(render(request, 'rake/index.html', context))
+    cost = time.time()
+    results, total_num = express_query(query_string, int(page), 10)
+    state = '为您找到 %d 条新闻，共耗时 %f 秒' % (total_num, time.time()-cost)
+
+    context = {
+        "raw_query_string": raw_query_string,
+        "query_string": query_string,
+        "current_page": int(page),
+        "total_page": range(0, 6),
+        "results": results,
+        'query_state': state
+    }
+    return HttpResponse(render(request, 'rake/news_list.html', context))
+    #
+    # context = {"items": range(1, 10)}
+    # return HttpResponse(render(request, 'rake/index.html', context))
